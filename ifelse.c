ed182236433245a7a85cd7eac2b0f5d71a9e1af6
Parinitha05 <parinithan2050@gmail.com>
2024-11-06 09:39:25 +0530

    nov 5

---
+++ ifelse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i;
+    scanf("%d",&i);
+    if(i%2==0){
+        printf("divisible by 2");
+    }
+    else{
+        printf("not divisible");
+    }
+    return 0;
+}
---
+++ switch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int d=1;
+    switch(d){
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednesday");
+        break;
+        default:
+        printf("invalid");
+    }
+}
