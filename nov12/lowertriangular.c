2d2bb4c99e2ab6ef87c27960470d2d4abe91c858
unknown <parinithan2050@gmail.com>
2024-11-12 16:09:31 +0530

    nov12

--- nov12/lowertriangular.c
+++ nov12/lowertriangular.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int isLower(int n,int arr[][n]){
     for(int i=0;i<n;i++){
-        for(int j=j+1;j<n;j++){
+        for(int j=i+1;j<n;j++){
              if(arr[i][j]!=0){
                 return 0;
              }
